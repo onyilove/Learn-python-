3eda3b70c6a15b77d4f8dd5db341523dbd888318
onyilove <154918804+onyilove@users.noreply.github.com>
2025-02-03 07:53:52 +0100

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,21 @@
+x=5
+y="John"
+print(x)
+print(y)
+x=4 # x is of type int
+x="Sally"# x is now of type str
+print(x)
+x=str(3)
+y= int(3)
+z= float(3)
+print(x)
+print(y)
+print(z)
+x=5
+y="John"
+print(type(x))
+print(type(y))
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
